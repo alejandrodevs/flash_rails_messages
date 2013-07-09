ea1d2552b31632e591b573959eece426f18bde04
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-07-09 11:53:40 -0500

    Adding basic methods to FlashRailsMessages helper

--- lib/flash_rails_messages.rb
+++ lib/flash_rails_messages.rb
@@ -4,6 +4,43 @@ module ActionView
   module Helpers
     module FlashRailsMessages
 
+      FLASH_TYPES = [:notice, :warning, :alert]
+
+      def render_messages
+        FLASH_TYPES.each do |type|
+          flash_messages << alert_element(type) if flash[type]
+          clean_flash_message(type)
+        end
+
+        flash_messages.html_safe
+      end
+
+      private
+
+      def clean_flash_message type
+        flash[type] = nil
+      end
+
+      def alert_element type
+        content_tag(:div, close_element + flash[type], :class => alert_classes(type))
+      end
+
+      def close_element
+        content_tag :span, "&times;".html_safe, :class => "close", :"data-dismiss" => "alert"
+      end
+
+      def alert_classes type
+        case type
+        when :notice  then "alert alert-success"
+        when :warning then "alert alert-warning"
+        when :alert   then "alert alert-error"
+        end
+      end
+
+      def flash_messages
+        @flash_messages ||= ""
+      end
+
     end
   end
 end
