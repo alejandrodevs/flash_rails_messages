c17341ba77aa4cdb5c600e05b1308a5084dd47ed
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-07-10 11:18:23 -0500

    Configurating spec_helper file

--- spec/flash_rails_messages_spec.rb
+++ spec/flash_rails_messages_spec.rb
@@ -1,6 +1,20 @@
 require 'spec_helper'
-require './lib/flash_rails_messages'
 
 describe ActionView::Helpers::FlashRailsMessagesHelper do
+  let!(:subject){ MockedClass.new }
 
+  describe "#flash_messages" do
+    context "when is not defined" do
+      it "returns a empty string" do
+        expect(subject.send(:flash_messages)).to eql("")
+      end
+    end
+
+    context "when is defined" do
+      it "returns the previous value" do
+        subject.send(:flash_messages) << "foo"
+        expect(subject.send(:flash_messages)).to eql("foo")
+      end
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
+require './lib/flash_rails_messages'
 require 'simplecov'
+
+class MockedClass
+  include ActionView::Helpers::FlashRailsMessagesHelper
+end
 
 SimpleCov.start do
   minimum_coverage 90
