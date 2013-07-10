b1fefba838563f287f37f4e32f1c45f44b4aa37c
Alejandro Gutiérrez <alejandrodevs@gmail.com>
2013-07-10 12:02:22 -0500

    Adding basic tests

--- spec/flash_rails_messages_spec.rb
+++ spec/flash_rails_messages_spec.rb
@@ -1,20 +1,55 @@
 require 'spec_helper'
 
 describe ActionView::Helpers::FlashRailsMessagesHelper do
-  let!(:subject){ MockedClass.new }
+  let!(:subject){ ActionView::Base.new }
 
-  describe "#flash_messages" do
-    context "when is not defined" do
-      it "returns a empty string" do
-        expect(subject.send(:flash_messages)).to eql("")
+  describe "#render_flash_messages" do
+    context "when flash does not have messages" do
+      it "returns nothing" do
+        subject.stub(:flash).and_return({})
+        expect(subject.render_flash_messages).to eql("")
       end
     end
 
-    context "when is defined" do
-      it "returns the previous value" do
-        subject.send(:flash_messages) << "foo"
-        expect(subject.send(:flash_messages)).to eql("foo")
+    context "when flash has messages" do
+      context "when flash type is notice" do
+        it "returns the correct message" do
+          subject.stub(:flash).and_return({:notice => "notice"})
+          expect(subject.render_flash_messages).to eql(alert_element("notice", "info"))
+        end
+      end
+
+      context "when flash type is success" do
+        it "returns the correct message" do
+          subject.stub(:flash).and_return({:success => "success"})
+          expect(subject.render_flash_messages).to eql(alert_element("success", "success"))
+        end
+      end
+
+      context "when flash type is alert" do
+        it "returns the correct message" do
+          subject.stub(:flash).and_return({:alert => "alert"})
+          expect(subject.render_flash_messages).to eql(alert_element("alert", "error"))
+        end
+      end
+
+      context "when has more than one message" do
+        it "returns all the correct messages" do
+          flash = {:alert => "alert", :notice => "notice"}
+          subject.stub(:flash).and_return(flash)
+          alerts_expected = alert_element("alert", "error") +
+                            alert_element("notice", "info")
+          expect(subject.render_flash_messages).to eql(alerts_expected)
+        end
       end
     end
+  end
+
+  def alert_element msg, type
+    subject.content_tag(:div, close_element + msg ,:class => "alert alert-#{type}")
+  end
+
+  def close_element
+    subject.content_tag(:span, "&times;".html_safe, :class => "close", :"data-dismiss" => "alert")
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
-require './lib/flash_rails_messages'
-require 'simplecov'
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-class MockedClass
-  include ActionView::Helpers::FlashRailsMessagesHelper
-end
+require 'action_view'
+require 'flash_rails_messages'
+require 'rails/all'
+require 'simplecov'
 
 SimpleCov.start do
   minimum_coverage 90
